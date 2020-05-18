22a6fde571f3fb81b2d6b2a01722d8c530528d0c
Philip Noebauer <pnoebauer>
2020-05-19 07:52:03 +0800

    made onUpdate function reusable for all fields

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,36 +9,31 @@ const Form = ( { route } ) => {
 		formName = 'Sign in';
 	}
 
-	const onUpdate = (event) => {
-		console.log(event.target.value);
+	const onUpdate = (event,type) => {
+		console.log(event.target.value,type);
 	}
 
 	return (
-		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-			<main className="pa4 black-80">
-			  <div className="measure">
+			<main className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center pa4 black-80">
+			  <form className="measure">
 			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 			      <legend className="f1 fw6 ph0 mh0">
 			      	{formName}
 			      </legend>
 			      {route==='Register' ? 
 			      	<div className="mt3">
-						<InputWithLabel type='text' name='name' id='name' onInputChange={onUpdate} >
+						<InputWithLabel type='text' name='name' id='name' onInputChange={(event) => onUpdate(event,'name')} >
 							Name
 						</InputWithLabel>
 					</div>
 			       : null}
-
 			       <div className="mt3">
-				        <div className="mt3">
-						<InputWithLabel type='email' name='email-address' id='email-address' onInputChange={onUpdate}>
+						<InputWithLabel type='email' name='email-address' id='email-address' onInputChange={(event) => onUpdate(event,'email')} >
 							Email
 						</InputWithLabel>
 					</div>
-				      </div>
-
 			      	<div className="mv3">
-		      			<InputWithLabel type='password' name='password' id='password' onInputChange={onUpdate}>
+		      			<InputWithLabel type='password' name='password' id='password' onInputChange={(event) => onUpdate(event,'password')} >
 							Password
 						</InputWithLabel>
 			      	</div>
@@ -50,10 +45,14 @@ const Form = ( { route } ) => {
 				      	value={formName}
 				      />
 				    </div>
-			  </div>
+			  </form>
 			</main>
-		</article>
 	)
 };
 
 export default Form;
+
+//comment: 
+//	onInputChange={onUpdate}
+//	same as:
+//	onInputChange={(e)=>onUpdate(e)}
